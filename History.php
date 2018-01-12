ddbca5d038248f3750b5eb4b462a47a21e57314a
CTBUH\ptuskevicius <ptuskevicius@ctbuh.org>
2018-01-12 09:01:51 -0600

    second

--- History.php
+++ History.php
@@ -23,5 +23,21 @@ class History extends Model {
 	public function model(){
 		return $this->morphTo();//->withTrashed();
 	}
+	
+	public function setUser($user){
+		$this->user()->associate($user);
+	}
+	
+	public function setModel($model){
+		$this->model()->associate($model);
+	}
+	
+	public function log($message, $type = 'custom', $extra = array() ){
+		$this->type = $type;
+		$this->data = $message;
+		$this->extra = $extra;
+		
+		return $this->save();
+	}
 }
 
---
+++ helpers.php
@@ -0,0 +1,8 @@
+<?php
+
+if(!function_exists('history')){
+	
+	function history(){
+		return new \ctbuh\History\History();
+	}
+}
